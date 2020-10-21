8c2f3a32e9d37649a67a4ce3b090acc31721c3a1
Vivian Weiss <weiss.wolfw3@gmail.com>
2020-10-21 00:44:11 -0500

    Moved Cactus Sword to ToolInit

--- src/main/java/net/vivi/ViviMain.java
+++ src/main/java/net/vivi/ViviMain.java
@@ -1,15 +1,12 @@
 package net.vivi;
 
 import net.fabricmc.api.ModInitializer;
-import net.minecraft.item.*;
-import net.minecraft.util.Identifier;
-import net.minecraft.util.registry.Registry;
 import net.vivi.init.CropInit;
 import net.vivi.init.FoodInit;
+import net.vivi.init.ToolInit;
 
 
 public class ViviMain implements ModInitializer {
-	public static final SwordItem CACTUS_SWORD = new CactusSword(CactusToolMaterial.INSTANCE, 0, -2.2f, new Item.Settings().group(ItemGroup.COMBAT));
 
 
 	@Override
@@ -17,6 +14,6 @@ public class ViviMain implements ModInitializer {
 		System.out.println("Vivi's food mod is starting!");
 		CropInit.init();
 		FoodInit.init();
-		Registry.register(Registry.ITEM, new Identifier("vivi", "cactus_sword"), CACTUS_SWORD);
+		ToolInit.init();
 	}
 }
--- src/main/java/net/vivi/init/ToolInit.java
+++ src/main/java/net/vivi/init/ToolInit.java
@@ -1,4 +1,17 @@
 package net.vivi.init;
 
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.SwordItem;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
+import net.vivi.CactusSword;
+import net.vivi.CactusToolMaterial;
+
 public class ToolInit {
+    public static final SwordItem CACTUS_SWORD = new CactusSword(CactusToolMaterial.INSTANCE, 0, -2.2f, new Item.Settings().group(ItemGroup.COMBAT));
+
+    public static void init() {
+        Registry.register(Registry.ITEM, new Identifier("vivi", "cactus_sword"), CACTUS_SWORD);
+    }
 }
