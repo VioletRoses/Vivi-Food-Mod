4f3c467ff5abc9e719b0ea33f3357410138ca18c
Vivian Weiss <weiss.wolfw3@gmail.com>
2020-10-21 00:42:54 -0500

    Removed Example Block

--- src/main/java/net/vivi/ViviMain.java
+++ src/main/java/net/vivi/ViviMain.java
@@ -1,9 +1,6 @@
 package net.vivi;
 
 import net.fabricmc.api.ModInitializer;
-import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
-import net.minecraft.block.Block;
-import net.minecraft.block.Material;
 import net.minecraft.item.*;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
@@ -12,7 +9,6 @@ import net.vivi.init.FoodInit;
 
 
 public class ViviMain implements ModInitializer {
-	public static final Block EXAMPLE_BLOCK = new Block(FabricBlockSettings.of(Material.WOOD).hardness(4.0f));
 	public static final SwordItem CACTUS_SWORD = new CactusSword(CactusToolMaterial.INSTANCE, 0, -2.2f, new Item.Settings().group(ItemGroup.COMBAT));
 
 
@@ -22,7 +18,5 @@ public class ViviMain implements ModInitializer {
 		CropInit.init();
 		FoodInit.init();
 		Registry.register(Registry.ITEM, new Identifier("vivi", "cactus_sword"), CACTUS_SWORD);
-		Registry.register(Registry.BLOCK, new Identifier("vivi", "example_block"), EXAMPLE_BLOCK);
-		Registry.register(Registry.ITEM, new Identifier("vivi", "example_block"), new BlockItem(EXAMPLE_BLOCK, new Item.Settings().group(ItemGroup.MISC)));
 	}
 }
---
+++ src/main/java/net/vivi/init/ToolInit.java
@@ -0,0 +1,4 @@
+package net.vivi.init;
+
+public class ToolInit {
+}
