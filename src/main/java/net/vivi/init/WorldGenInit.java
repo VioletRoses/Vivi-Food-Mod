78f6dc02382a4f5ac1cdb97d1749ae1881592e6d
Vivian Weiss <weiss.wolfw3@gmail.com>
2020-10-21 15:02:13 -0500

    Increased salt vein size, added salt block recipe, salt blocks now drop salt

--- src/main/java/net/vivi/init/WorldGenInit.java
+++ src/main/java/net/vivi/init/WorldGenInit.java
@@ -11,7 +11,7 @@ import net.minecraft.world.gen.feature.OreFeatureConfig;
 
 public class WorldGenInit {
 
-    public static ConfiguredFeature<?, ?> ORE_SALT_OVERWORLD = Feature.ORE.configure(new OreFeatureConfig(OreFeatureConfig.Rules.BASE_STONE_OVERWORLD, BlockInit.SALT_BLOCK.getDefaultState(), 9)).decorate(Decorator.RANGE.configure(new RangeDecoratorConfig(0, 0, 64)).spreadHorizontally().repeat(15));
+    public static ConfiguredFeature<?, ?> ORE_SALT_OVERWORLD = Feature.ORE.configure(new OreFeatureConfig(OreFeatureConfig.Rules.BASE_STONE_OVERWORLD, BlockInit.SALT_BLOCK.getDefaultState(), 27)).decorate(Decorator.RANGE.configure(new RangeDecoratorConfig(0, 0, 64)).spreadHorizontally().repeat(15));
 
     public static void init() {
         Registry.register(BuiltinRegistries.CONFIGURED_FEATURE, new Identifier("vivi", "ore_salt_overworld"), ORE_SALT_OVERWORLD);
