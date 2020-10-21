045c93fe5b29c81aaceec7df1c0fed5db4f1e4f1
Vivian Weiss <weiss.wolfw3@gmail.com>
2020-10-21 17:50:10 -0500

    Added salt texture, renamed block to salt ore

--- src/main/java/net/vivi/init/BlockInit.java
+++ src/main/java/net/vivi/init/BlockInit.java
@@ -11,13 +11,13 @@ import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 
 public class BlockInit {
-    public static final Block SALT_BLOCK = new Block(FabricBlockSettings.of(Material.SOIL).hardness(0.5f));
+    public static final Block SALT_ORE = new Block(FabricBlockSettings.of(Material.SOIL).hardness(0.5f));
     public static final Item SALT = new Item(new FabricItemSettings().group(ItemGroup.FOOD));
 
     public static void init() {
         //Salt
         Registry.register(Registry.ITEM, new Identifier("vivi", "salt"), SALT);
-        Registry.register(Registry.BLOCK, new Identifier("vivi", "salt_block"), SALT_BLOCK);
-        Registry.register(Registry.ITEM, new Identifier("vivi", "salt_block"), new BlockItem(SALT_BLOCK, new Item.Settings().group(ItemGroup.BUILDING_BLOCKS)));
+        Registry.register(Registry.BLOCK, new Identifier("vivi", "salt_ore"), SALT_ORE);
+        Registry.register(Registry.ITEM, new Identifier("vivi", "salt_ore"), new BlockItem(SALT_ORE, new Item.Settings().group(ItemGroup.BUILDING_BLOCKS)));
     }
 }
